c9d032696d31aedfcf7e9f6e5484597dcd926bcf
Juan Manuel Ibañez <jmiban@gmail.com>
2019-03-23 17:54:24 -0300

    admin interface maqueteado

---
+++ front/src/components/OrderList.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+    Jumbotron,
+    ButtonToolbar,
+    Button,
+    Container,
+    Row,
+    Col,
+    Dropdown,
+
+} from 'react-bootstrap';
+
+import OrderListTable from './OrderListTable.jsx'
+
+const compraTrucha = [{
+    orderDesc: "xxxxx-yyyy-qqqq",
+    status: "En progreso",
+    direccion: "Avenida Siempre Vivas 234",
+    mail: "piphole@mail.com"
+
+},
+{
+    orderDesc: "KKKKK-PPP-EEE",
+    status: "cerrado",
+    direccion: "Paranä 444",
+    mail: "sanchin@mail.com"
+
+}];
+
+export default () => (
+    <Jumbotron>
+        <h1>Order management</h1>
+        <ButtonToolbar>
+            <Container>
+                <Row>
+                    <Col>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                                Filter by Status
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item >Action</Dropdown.Item>
+                                <Dropdown.Item >Another action</Dropdown.Item>
+                                <Dropdown.Item >Something else</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
+                    <Col>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                                Change status
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item >Action</Dropdown.Item>
+                                <Dropdown.Item >Another action</Dropdown.Item>
+                                <Dropdown.Item >Something else</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown></Col>
+                    <Col><Button variant="danger">Delete</Button></Col>
+                    <Col><Button variant="primary">New order</Button></Col>
+                </Row>
+                <Row>
+                    <OrderListTable orderList={compraTrucha}/>
+                </Row>
+                <Button variant="primary">Save</Button>
+            </Container>
+        </ButtonToolbar>
+    </Jumbotron>
+)
--- front/src/containers/OrderManagement.jsx
+++ front/src/containers/OrderManagement.jsx
@@ -1,45 +1,48 @@
 
 import React from 'react';
-import { Button, ButtonGroup, Jumbotron, Row, Col, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Button, ButtonGroup, Row, Col, Container, Form, Jumbotron, Tab, Nav, } from 'react-bootstrap';
+import OrderList from '../components/OrderList'
+
 
 class OrderManagement extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
+        this.state = {
+            componentID: 1,
+        }
+    }
 
-  }
+    render() {
+        return (
+
+
+            <div>
+
+                <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                    <Row>
+                        <Col sm={3}>
+                            <Nav variant="pills" className="flex-column">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Create Product</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col sm={9}>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <OrderList />
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>
+    </div>
 
 
 
 
-  render() {
-    return (
-      <div className="adminPanel">
-        <Container>
-          <Row class="d-flex p-2">
-            <Col>
-              <ButtonGroup vertical>
-
-                <Link to="/admin/productManagement"><Button>Product management</Button></Link>
-                <Link to="/admin/userManagement"><Button >User management</Button></Link>
-                <Link to="/admin/orderManagement"> <Button>Order management</Button></Link>
-
-              </ButtonGroup>
-            </Col>
-            <Col>
-              <Jumbotron>
-                <h1>Order management</h1>
-                <p>
-                  Welcome to admin user dashboard
-                </p>
-              </Jumbotron>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-
-    );
-  }
+        );
+    }
 }
 
 export default OrderManagement;

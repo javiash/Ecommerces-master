24663234e88755e249ce5a9463eb5e990e590a76
Javier <javilanger@gmail.com>
2019-03-24 16:56:47 -0300

    mnasd

--- back/routes/cartRoutes.js
+++ back/routes/cartRoutes.js
@@ -3,6 +3,19 @@ const Cart = require('../models/shopcart');
 const Book = require('../models/book')
 const { bookPurchase, bookCategory } = require('../models/index-models');
 
+router.post('/newbook', (req, res) => {
+  bookCategory.Book.create({
+    name: 'Game of Thrones',
+    author: 'George R. R. Martin',
+    year: '6 de agosto de 1996',
+    editorial: 'Bantam Spectra',
+    description: 'Se trata de la primera entrega de la serie de gran popularidad Canción de hielo y fuego. La novela se caracteriza por su estética medieval, la descripción de numerosos personajes bien detallados, la contraposición de puntos de vista de los múltiples protagonistas, su trama con giros inesperados y un uso sutil y moderado de los aspectos mágicos tan comunes en otras obras de fantasía heroica.',
+    sold: 15400,
+    price: 1500,
+    stock: 200,
+  })
+})
+
 
 router.get('/:id', (req, res) => {
   Cart.findOne({ where: { userId: req.params.id } })
